ddde2155034ed1c775b135b4bdc4167a268a7f42
Jonathan Jie <jonathan@scrio.ai>
2022-09-16 10:57:03 +0000

    added noon report schema

--- marinanet/enums.py
+++ marinanet/enums.py
@@ -9,3 +9,21 @@ class Status(models.IntegerChoices):
 class ShipAccessPrivilege(models.IntegerChoices):
     READ = 0, _("Read only access")
     WRITE = 1, _("Read and Write access")
+
+class ShipTypes(models.TextChoices):
+    CONTAINER = "CNTR", _("Container Ship")
+    BULK_CARRIER = "BULK", _("Bulk Carrier")
+    OIL_TANKER = "OIL", _("Oil Tanker")
+    GAS_TANKER = "GAS", _("Gas/LNG Tanker")
+    RORO = "RORO", _("Roll-On Roll-Off Ships")
+    
+class ReportTypes(models.TextChoices):
+    NOON = "NOON", _("Noon")
+    DEPARTURE = "DEP", _("Departure")
+    ARRIVAL = "ARR", _("Arrival")
+    BDN = "BDN", _("Bunker Delivery Note")
+    
+class NoonReportTypes(models.TextChoices):
+    NOON_SEA = "SEA", _("Noon at Sea")
+    NOON_WAITING = "WAIT", _("Noon Waiting")
+    NOON_IN_PORT = "PORT", _("Noon in Port")
