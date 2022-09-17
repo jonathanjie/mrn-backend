0edb3c5c5847b4745ffadf180e61f01bfc5753c8
Jonathan Jie <jonathan@scrio.ai>
2022-09-17 14:17:00 +0000

    fixed formatting + test api

--- marinanet/views.py
+++ marinanet/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from functools import wraps
 
-# Create your views here.
+from django.http import JsonResponse
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
+def test(request):
+    return JsonResponse({'message': 'TESTED'})
