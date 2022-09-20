01a35e84059b0dae0ed075716f7ab9152106f0cc
daniel <daniel@marinachain.io>
2022-09-20 18:30:25 +0800

    Modified Noon Report models

--- marinanet/enums.py
+++ marinanet/enums.py
@@ -27,3 +27,59 @@ class ReportTypes(models.TextChoices):
     DEPARTURE = "DEP", _("Departure")
     ARRIVAL = "ARR", _("Arrival")
     BDN = "BDN", _("Bunker Delivery Note")
+
+
+class CargoPresence(models.TextChoices):
+    BALLAST = "BALL", _("Ballast")
+    LADEN = "LADN", _("Laden")
+    EASTBOUND = "EAST", _("Eastbound")
+    WESTBOUND = "WEST", _("Westbound")
+
+
+class Weather(models.TextChoices):
+    BLUE_SKY = "B", _("Blue sky (Cloud 0-2)")
+    FINE_CLOUDY = "BC", _("Fine but Cloudy (Cloud 3-7)")
+    CLOUDY = "C", _("Cloudy (Cloud 8-10)")
+    DRIZZLING = "D", _("Drizzling rain")
+    FOG = "F", _("Fog")
+    GLOOM = "G", _("Gloom")
+    HAIL = "H", _("Hail")
+    LIGHTNING = "L", _("Lightning")
+    MIST = "M", _("Mist")
+    OVERCAST = "O", ("Overcast (Cloud 10)")
+    PASSING_SHOWERS = "P", _("Passing showers")
+    SQUALLS = "Q", _("Squalls")
+    RAIN = "R", _("Rain")
+    SNOW = "S", _("Snow")
+    THUNDER = "T", _("Thunder")
+    UGLY "U", _("Ugly threatening weather")
+    DEW = "W", _("Dew")
+    HAZE = "Z", _("Haze")
+
+
+class Beaufort(models.IntegerChoices):
+    CALM = 0, _("Calm")
+    LIGHT_AIR = 1, _("Light air")
+    LIGHT_BREEZE = 2, _("Light breeze")
+    GENTLE_BREEZE = 3, _("Gentle breeze")
+    MODERATE_BREEZE = 4, ("Moderate breeze")
+    FRESH_BREEZE = 5, _("Fresh breeze")
+    STRONG_BREEZE = 6, _("Strong breeze")
+    NEAR_GALE = 7, _("Near gale")
+    GALE = 8, _("Gale")
+    STRONG_GALE = 9, _("Strong gale")
+    STORM = 10, _("Storm")
+    VIOLENT_STORM = 11, _("Violent storm")
+    HURRICANE = 12, _("Hurricane")
+
+
+class FuelType(models.TextChoices):
+    HFO = "HFO", _("HFO")
+    LSFO = "LSFO" _("LSFO")
+    MDO = "MDO", _("MDO")
+    MGO = "MGO", _("MGO")
+    LPG_PROPANE = "LPGP", _("LPG (Propane)")
+    LPG_BUTANE = "LPGB", _("LPG (Butane")
+    METHANOL = "METH", _("Methanol")
+    ETHANOL = "ETH", _("Ethanol")
+    LNG = "LNG", _("LNG")
--- marinanet/models.py
+++ marinanet/models.py
@@ -5,10 +5,21 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.gis.db import models
 
 from auth0.models import User
-from marinanet.enums import ReportTypes, ShipAccessPrivilege, ShipTypes, Status
+from marinanet.enums import (
+    Beaufort,
+    CargoPresence,
+    FuelType
+    ReportTypes,
+    ShipAccessPrivilege,
+    ShipTypes,
+    Status,
+    Weather,
+)
 
 
-### USERS ###
+""" USER MODELS
+"""
+
 
 class Company(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
@@ -39,7 +50,7 @@ class Profile(models.Model):
 class Ship(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     name = models.CharField(max_length=255)
-    imo_reg = models.PositiveIntegerField()
+    imo_reg = models.PositiveIntegerField()     # 7 digits
     company = models.ForeignKey(Company, on_delete=models.PROTECT)
     ship_type = models.CharField(max_length=4, choices=ShipTypes.choices)
     assigned_users = models.ManyToManyField(
@@ -67,7 +78,9 @@ class ShipUser(models.Model):
     class Meta:
         db_table = "ship_users"
 
-### REPORTS ###
+
+""" REPORT MODELS
+"""
 
 
 class Voyage(models.model):
@@ -91,9 +104,7 @@ class ReportHeader(models.model):
     voyage = models.ForeignKey(Voyage, on_delete=models.PROTECT)
     report_type = models.ChoiceField(choices=ReportTypes.choices)
     report_num = models.IntegerField()
-    cargo_presence = models.BooleanField()
-    timezone = models.IntegerField()
-    datetime = models.DateTimeField()
+    cargo_presence = models.TextField(max_length=4, choices=CargoPresence)
     summer_time = models.BooleanField()
     position = models.PointField(srid=4326)
     # Note that for position, X is Longitude, Y is Lattitude
@@ -114,61 +125,62 @@ class ReportData(models.Model):
 
 
 class NoonReportAtSea(ReportData):
-    distance_to_standby = models.DecimalField(
+    distance_to_go = models.DecimalField(
+        max_digits=5,
+        decimal_places=0,
         validators=[MinValueValidator(Decimal("0.0"))])
     hours_since_noon = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
+        max_digits=3,
+        decimal_places=1,
+        validators=[
+            MinValueValidator(Decimal("0.0")),
+            MaxValueValidator(Decimal("48"))])
     hours_total = models.DecimalField(
+        max_digits=5,
+        decimal_places=1,
         validators=[MinValueValidator(Decimal("0.0"))])
     distance_obs_since_noon = models.DecimalField(
+        max_digits=3,
+        decimal_places=0,
         validators=[MinValueValidator(Decimal("0.0"))])
     distance_eng_since_noon = models.DecimalField(
+        max_digits=3,
+        decimal_places=0,
         validators=[MinValueValidator(Decimal("0.0"))])
     distance_eng_total = models.DecimalField(
+        max_digits=5,
+        decimal_places=0,
         validators=[MinValueValidator(Decimal("0.0"))])
     revolution_count = models.IntField(
-        validators=[MinValueValidator(Decimal("0.0"))])
+        validators=[MinValueValidator(0)])
     speed_since_noon = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
         validators=[MinValueValidator(Decimal("0.0"))])
     rpm_since_noon = models.DecimalField(
+        max_digits=4,
+        decimal_places=1,
         validators=[MinValueValidator(Decimal("0.0"))])
     slip_since_noon = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
         validators=[MinValueValidator(Decimal("0.0"))])
     speed_avg = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
         validators=[MinValueValidator(Decimal("0.0"))])
     rpm_avg = models.DecimalField(
+        max_digits=4,
+        decimal_places=1,
         validators=[MinValueValidator(Decimal("0.0"))])
     slip_avg = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
         validators=[MinValueValidator(Decimal("0.0"))])
-    weather = models.CharField(max_length=255)
-    sea_state = models.PositiveSmallIntegerField(
-        validators=[MinValueValidator(0)])
-    wind_direction = models.DecimalField(
-        validators=[
-            MinValueValidator(Decimal("0.0")),
-            MaxValueValidator(Decimal("360"))
-            ])
-    wind_speed = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    heavy_weather_hours = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    heavy_weather_dist = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    heavy_weather_foc = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    heavy_weather_wind_direction = models.DecimalField(
-        validators=[
-            MinValueValidator(Decimal("0.0")),
-            MaxValueValidator(Decimal("360"))
-            ])
-    heavy_weather_wind_speed = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    heavy_weather_max_wave_ht = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    changed_speed = models.DecimalField()
-    changed_foc = models.DecimalField()
-    changed_rpm = models.DecimalField()
-    changed_date = models.DecimalField()
+    # changed_speed = models.DecimalField()
+    # changed_foc = models.DecimalField()
+    # changed_rpm = models.DecimalField()
+    # changed_date = models.DecimalField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
@@ -176,52 +188,122 @@ class NoonReportAtSea(ReportData):
         db_table = "noon_reports_at_sea"
 
 
-class BunkerDetails(models.model):
-    report_data = models.OneToOneField(ReportData, on_delete=models.PROTECT)
-    consumed_lsfo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    consumed_mgo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    consumed_correction = models.CharField()
-    consumed_remarks = models.CharField()
-    received_lsfo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    received_lsfo_time = models.DateTimeField()
-    received_mgo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    received_mgo_time = models.DateTimeField()
-    received_meslo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    received_meclo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    received_geslo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    received_freshwater = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    received_correction = models.CharField()
-    received_remark = models.CharField()
-    rob_lsfo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    rob_mgo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    rob_freshwater = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    rob_meslo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    rob_mest = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    rob_meslo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    rob_ meclo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    rob_ geslo = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    evaporated_freshwater = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
-    consumed_freshwater = models.DecimalField(
-        validators=[MinValueValidator(Decimal("0.0"))])
+class WeatherData(models.Model):
+    report_data = models.OneToOneField(
+        ReportData, on_delete=models.PROTECT, primary_key=True)
+    weather = models.TextField(max_length=2, choices=Weather.choices)
+    sea_state = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(9)])
+    wind_direction = models.DecimalField(
+        max_digits=4,
+        decimal_places=1,
+        validators=[
+            MinValueValidator(Decimal("0.0")),
+            MaxValueValidator(Decimal("360"))])
+    wind_speed = models.DecimalField(
+        max_digits=4,
+        decimal_places=1,
+        validators=[
+            MinValueValidator(Decimal("0.0"))])
+    beaufort = models.PositiveSmallIntegerField(choices=Beaufort.choices)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = "report_data_bunker_details"
+        db_table = "weather_data"
+
+
+class HeavyWeatherData(models.Model):
+    report_data = models.OneToOneField(
+        ReportData, on_delete=models.PROTECT, primary_key=True)
+    heavy_weather_hours = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        validators=[MinValueValidator(Decimal("0.0"))])
+    heavy_weather_dist = models.DecimalField(
+        max_digits=3,
+        decimal_places=0,
+        validators=[MinValueValidator(Decimal("0.0"))])
+    heavy_weather_consumption = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.0"))])
+    heavy_weather_wind_direction = models.DecimalField(
+        max_digits=4,
+        decimal_places=1,
+        validators=[
+            MinValueValidator(Decimal("0.0")),
+            MaxValueValidator(Decimal("360"))])
+    heavy_weather_wind_speed = models.DecimalField(
+        max_digits=4,
+        decimal_places=1,
+        validators=[
+            MinValueValidator(Decimal("0.0"))])
+    heavy_weather_max_wave_ht = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        validators=[
+            MinValueValidator(Decimal("0.0"))])
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "heavy_weather_data"
+
+
+class BunkerData(models.Model):
+    report_data = models.ForeignKey(
+        ReportData, on_delete=models.PROTECT)
+    fuel_type = models.TextField(max_length=4, choices=FuelType.choices)
+    rob = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.0"))])
+    me_consumed = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.0"))])
+    aux_consumed = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.0"))])
+    boiler_consumed = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.0"))])
+    gas_generator_consumed = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.0"))])
+    total_consumed = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.0"))])
+    correction = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.0"))])
+    remarks = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "bunker_data"
+        unique_together = ["report_data", "fuel_type"]
+
+
+class FreshWaterData(models.Model):
+    report_data = models.OneToOneField(
+        ReportData, on_delete=models.PROTECT, primary_key=True)
+    rob = models.PositiveIntegerField()
+    consumed = models.PositiveIntegerField()
+    evaporated = models.PositiveIntegerField()
+    correction = models.IntegerField()
+    remarks = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "fresh_water_data"
