232ef36dbac4779f04793328dbcbc2404836c4f6
daniel <daniel@marinachain.io>
2023-01-08 18:15:06 +0800

    Add leg data to report headers list

--- marinanet/serializers/model_serializers.py
+++ marinanet/serializers/model_serializers.py
@@ -158,7 +158,7 @@ class VoyageReportsSerializer(serializers.ModelSerializer):
         report_headers = []
         for voyage_leg in voyage_legs:
             report_headers.extend(voyage_leg.reportheader_set.all())
-        serializer = ReportHeaderSerializer(report_headers, many=True)
+        serializer = ReportHeaderWtihLegSerializer(report_headers, many=True)
         return serializer.data
 
 
