a08a55b13b584a980eba0f1ec1deb87cf9bc1a8a
daniel <daniel@marinachain.io>
2023-01-26 13:09:48 +0800

    Fix api paths

--- auth0/urls.py
+++ auth0/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/public', views.public),
-    path('api/private', views.private),
-    path('api/private-scoped', views.private_scoped),
+    path('public', views.public),
+    path('private', views.private),
+    path('private-scoped', views.private_scoped),
 ]
