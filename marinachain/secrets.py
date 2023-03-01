34bfccaa6eae3a7ba05daa33b48cdb3d6f1360eb
XP Liao <mechastriker3@gmail.com>
2023-03-01 16:25:44 +0800

    commit

---
+++ marinachain/secrets.py
@@ -0,0 +1,14 @@
+# Django Secrets
+DJANGO_SECRET_KEY = "django-insecure-9p%xcacn9qwe%z&o^4whc&$&g1luj1ru%3-=yg6*0*=bk)4w!u"
+
+# DB Secrets
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME = "marinanet"
+DB_USER = "marinanet"
+DB_PASSWORD = "correcthorsebatterystaple"
+
+
+# Auth0 Secrets
+JWT_AUDIENCE = "https://django-jwt-test-dan/api"
+JWT_ISSUER = "dev-xyrhs609.eu.auth0.com"
